1fe3b9697e06362d106928abafcf3067a1e99991
mohdshahzil <theshahzil@gmail.com>
2024-01-27 18:54:41 +0530

    image-added

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-
+"use client"
+import React from "react";
+import Image from "next/image";
+import msbww from "../public/assets/msbww.jpg";
 export default function Header() {
   return (
-    <div className='flex items-center justify-between ' >
-      <div className='w-3/5 text-7xl ' >left</div>
-      <div className='w-2/5 text-3xl' >right</div>
+    <div className="flex flex-col md:flex-row items-center justify-between gap-3 md:p-8 p-4 ">
+      <div className="w-3/5 md:text-5xl text-4xl font-bold ">Hey! This is Shahzil</div>
+      
+      <div className="w-2/5 text-3xl items-end flex justify-center">
+        <Image alt="Mohammad Shahzil" className="rounded-xl" height={300} src={msbww} />
+      </div>
     </div>
-  )
+  );
 }
