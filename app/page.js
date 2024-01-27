8ff1e42d00d3e35eacbd8907632406eda581f0e2
mohdshahzil <theshahzil@gmail.com>
2024-01-27 16:34:14 +0530

    appbar imported

--- app/page.js
+++ app/page.js
@@ -1,9 +1,39 @@
-import React from 'react'
-
+import React from "react";
+import Appbar from "@/components/Appbar";
+import Header from "@/components/Header";
+import { Divider } from "@nextui-org/react";
 const page = () => {
   return (
-    <div>page</div>
-  )
-}
+    <div className="h-full w-full overflow-y ">
+      <div className="">
+        <Appbar />
+      </div>
+      <div className="flex items-center justify-center">
+        <div className="w-full md:w-4/6 lg:w-4/6">
+          <Divider className="my-4" />
+          <Header />
+          <h1 className="text-8xl">hey</h1>
+          <h1 className="text-8xl">hey</h1>
+          <h1 className="text-8xl">hey</h1>
+          <h1 className="text-8xl">hey</h1>
+          <h1 className="text-8xl">hey</h1>
+          <h1 className="text-8xl">hey</h1>
+          <h1 className="text-8xl">hey</h1>
+          <h1 className="text-8xl">hey</h1>
+          <h1 className="text-8xl">hey</h1>
+          <h1 className="text-8xl">hey</h1>
+          <h1 className="text-8xl">hey</h1>
+          <h1 className="text-8xl">hey</h1>
+          <h1 className="text-8xl">hey</h1>
+          <h1 className="text-8xl">hey</h1>
+          <h1 className="text-8xl">hey</h1>
+          <h1 className="text-8xl">hey</h1>
+          <h1 className="text-8xl">hey</h1>
+          <h1 className="text-8xl">hey</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default page
+export default page;
